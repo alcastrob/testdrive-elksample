e8ee1c99cf85c83915f56da6ad8b5209a50b87cb
Angel L. Castro <al.castro@gmail.com>
2018-04-29 11:53:19 +0200

    Source code

---
+++ DotnetELKSample/LogEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElkSample
+{
+    public class LogEntity
+    {
+        public string StringField { get; set; }
+        public int IntField { get; set; }
+        public long LongField { get; set; }
+    }
+}
---
+++ DotnetELKSample/Program.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using StatsdClient;
+using SyslogNet.Client;
+using SyslogNet.Client.Serialization;
+using SyslogNet.Client.Transport;
+using System;
+
+namespace ElkSample
+{
+    class Program
+    {
+        //TODO: Update your ELK instance IP addresses and ports here
+        private const string syslogAddress = "192.168.0.160";
+        private const int syslogPort = 3514;
+        private const string statsdAddress = "192.168.0.160";
+        private const int statsdPort = 6000;
+
+        static void Main(string[] args)
+        {            
+            UsingSyslog();
+            //UsingStatsd();
+        }
+
+        private static void UsingSyslog()
+        {
+            var payload = new LogEntity
+            {
+                IntField = 7,
+                LongField = 1200400,
+                StringField = "string sample",
+            };
+            
+            ISyslogMessageSender sender = new SyslogUdpSender(syslogAddress, syslogPort);
+            ISyslogMessageSerializer serializer = new SyslogRfc5424MessageSerializer();
+
+            var msg = new SyslogMessage(
+                DateTimeOffset.Now, //DatetimeOffset
+                Facility.UserLevelMessages, //Facility
+                Severity.Debug, //Severity
+                Environment.MachineName, //HostName
+                "BPTest", //Appname
+                "0", //ProcId
+                "0", //MsgId
+                JsonConvert.SerializeObject(payload) //Message
+                );
+
+            for (int counter = 0; counter < 100; counter++)
+            {
+                sender.Send(msg, new SyslogRfc5424MessageSerializer());
+            }
+        }
+
+        private static void UsingStatsd()
+        {
+            Metrics.Configure(new MetricsConfig
+            {
+                StatsdServerName = statsdAddress,                
+                StatsdServerPort = statsdPort,
+                Prefix = "PrefixSample"
+            });
+
+            var msgPayload = new
+            {
+                origin = "Origin Sample",
+                renderTime = DateTime.UtcNow
+            };
+
+            for (int counter = 0; counter < 100; counter++)
+            {
+                Metrics.Set("Bulkdata", //Name of the statds containers
+                    JsonConvert.SerializeObject(msgPayload)); //Object to send
+            }
+        }
+    }
+}
