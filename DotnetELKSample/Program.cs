fcfa9ba88b418ac041855064326b32fbac973f4b
Angel L. Castro <al.castro@gmail.com>
2018-06-20 13:53:52 +0200

    Added Autorization via nginx

--- DotnetELKSample/Program.cs
+++ DotnetELKSample/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Newtonsoft.Json;
-using StatsdClient;
 using SyslogNet.Client;
 using SyslogNet.Client.Serialization;
 using SyslogNet.Client.Transport;
@@ -10,9 +9,9 @@ namespace ElkSample
     class Program
     {
         //TODO: Update your ELK instance IP addresses and ports here
-        private const string syslogAddress = "192.168.0.160";
+        private const string syslogAddress = "159.89.5.228";
         private const int syslogPort = 3514;
-        private const string statsdAddress = "192.168.0.160";
+        private const string statsdAddress = "159.89.5.228";
         private const int statsdPort = 6000;
 
         static void Main(string[] args)
@@ -27,7 +26,7 @@ namespace ElkSample
             {
                 IntField = 7,
                 LongField = 1200400,
-                StringField = "string sample",
+                StringField = "Connection error with endpoint",
             };
             
             ISyslogMessageSender sender = new SyslogUdpSender(syslogAddress, syslogPort);
@@ -38,37 +37,15 @@ namespace ElkSample
                 Facility.UserLevelMessages, //Facility
                 Severity.Debug, //Severity
                 Environment.MachineName, //HostName
-                "BPTest", //Appname
+                "Open", //Appname
                 "0", //ProcId
                 "0", //MsgId
                 JsonConvert.SerializeObject(payload) //Message
                 );
 
-            for (int counter = 0; counter < 100; counter++)
+            for (int counter = 0; counter < 1; counter++)
             {
                 sender.Send(msg, new SyslogRfc5424MessageSerializer());
-            }
-        }
-
-        private static void UsingStatsd()
-        {
-            Metrics.Configure(new MetricsConfig
-            {
-                StatsdServerName = statsdAddress,                
-                StatsdServerPort = statsdPort,
-                Prefix = "PrefixSample"
-            });
-
-            var msgPayload = new
-            {
-                origin = "Origin Sample",
-                renderTime = DateTime.UtcNow
-            };
-
-            for (int counter = 0; counter < 100; counter++)
-            {
-                Metrics.Set("Bulkdata", //Name of the statds containers
-                    JsonConvert.SerializeObject(msgPayload)); //Object to send
             }
         }
     }
